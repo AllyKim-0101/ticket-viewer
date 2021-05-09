14acb8d4585b84687db7f98ab0322bc0e3068e8e
Ally Kim <57968966+AllyKim-0101@users.noreply.github.com>
2021-05-09 18:15:31 +1000

    1.connect to zendesk API 2.request all the tickets for my account

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+const fetch = require("node-fetch");
+var express = require("express");
 var router = express.Router();
 
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
+let url = `https://${username}:${password}@allykim.zendesk.com/api/v2/tickets.json`;
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((parsedData) => {
+      console.log(parsedData);
+      res.render("index", {
+        title: "Zendesk Ticket Viewer",
+        numberOfTicket: parsedData.count,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
