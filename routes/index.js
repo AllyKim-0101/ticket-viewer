8cbf6774eaff6fc5e9e9762f34cec3ee726bdd70
Ally Kim <57968966+AllyKim-0101@users.noreply.github.com>
2021-05-09 19:55:28 +1000

    display friendly error message to the user

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,20 @@ router.get("/", function (req, res, next) {
       return data.json();
     })
     .then((parsedData) => {
-      console.log(parsedData);
       res.render("index", {
         title: "Zendesk Ticket Viewer",
         numberOfTicket: parsedData.count,
+        // diplay error message if parsedData contains error otherwise dont ,which means error in js will be undefined(=no error) 
+        error: parsedData.error ? `Opps, We are so sorry :( Something went wrong: ${parsedData.error}` : undefined
       });
     })
+    //if we dont receive parsed data then disply an error to the user
     .catch((error) => {
-      console.log(error);
+      res.render("index", {
+        title: "Zendesk Ticket Viewer",
+        error: `Opps, we could not retrive the information from Zendesk. Here is what went wrong: ${error}`
+      });
+
     });
 });
 
