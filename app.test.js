cc6e6abac83e43024ad6f19d4446256662bef5a7
Ally Kim <57968966+AllyKim-0101@users.noreply.github.com>
2021-05-15 19:02:19 +1000

    draw skeleton of tests

--- app.test.js
+++ app.test.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const zendeskAPI = require("./lib/zendeskAPI");
 const request = require("supertest");
+const { response } = require("express");
 const parser = new DOMParser();
 
 jest.mock("./lib/zendeskAPI", () => {
@@ -11,22 +12,26 @@ jest.mock("./lib/zendeskAPI", () => {
 });
 
 describe("homepage", () => {
+  test("returns 200 status code", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toEqual(200);
+  });
+
   test("shows the first 25 tickets", async () => {
     const response = await request(app).get("/");
-    expect(response.statusCode).toBe(200);
-
     const document = parser.parseFromString(response.text, "text/html");
     expect(document.querySelectorAll("li").length).toBe(25);
   });
 
-  test("shows the number of available tickets", () => {
-    return request(app)
-      .get("/")
-      .then((response) => {
-        const document = parser.parseFromString(response.text, "text/html");
-        expect(document.querySelector("#numberOfTicket").innerHTML).toEqual(
-          "102"
-        );
-      });
+  test("shows the number of available tickets", async () => {
+    const response = await request(app).get("/");
+    const document = parser.parseFromString(response.text, "text/html");
+    expect(document.querySelector("#numberOfTicket").innerHTML).toEqual("102");
   });
+});
+
+describe("ticket details", () => {
+  test.todo("returns 200 status code");
+  test.todo("shows the ticket subject");
+  test.todo("shows the ticket description");
 });
