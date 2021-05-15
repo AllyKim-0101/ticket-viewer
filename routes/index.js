8a8c1889f3868de4128d0dddcf8b058950907621
Ally Kim <57968966+AllyKim-0101@users.noreply.github.com>
2021-05-15 15:20:53 +1000

    implement pagination

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,9 @@ const password = process.env.PASSWORD;
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const url = zendeskAPI.buildURL(username, password)
-  fetch(`${url}tickets.json`)
+  const url = zendeskAPI.buildURL(username, password);
+  let pageNumber = parseInt(req.query.page) || 1;
+  fetch(`${url}tickets.json?page=${pageNumber}&per_page=25`)
     .then((data) => {
       return data.json();
     })
@@ -18,15 +19,18 @@ router.get("/", function (req, res, next) {
         title: "Zendesk Ticket Viewer",
         numberOfTicket: parsedData.count,
         list: parsedData.tickets,
-        // diplay error message if parsedData contains error otherwise dont ,which means error in js will be undefined(=no error) 
-        error: parsedData.error ? `Opps, We are so sorry :( Something went wrong: ${parsedData.error}` : undefined
+        nextPage: parsedData.tickets.length < 25 ? undefined : pageNumber + 1,
+        // diplay error message if parsedData contains error otherwise dont ,which means error in js will be undefined(=no error)
+        error: parsedData.error
+          ? `Opps, We are so sorry :( Something went wrong: ${parsedData.error.title} (${parsedData.error.message})`
+          : undefined,
       });
     })
     //if we dont receive parsed data then disply an error to the user
     .catch((error) => {
       res.render("index", {
         title: "Zendesk Ticket Viewer",
-        error: `Opps, we could not retrive the information from Zendesk. Here is what went wrong: ${error}`
+        error: `Opps, we could not retrive the information from Zendesk. Here is what went wrong: ${error}`,
       });
     });
 });
